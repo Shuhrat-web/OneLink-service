ebe8720eae616fb804fbba829afccf3d75b22cc0
Shuhrat-web <s.sh.r.demon@gmail.com>
2026-04-10 15:27:54 +0500

    added favicon metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,15 @@ const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"]
 export const metadata: Metadata = {
   title: "SmartLink / OneQR Platform",
   description: "Smart redirect platform with QR and analytics",
+  // icons: {
+  //   icon: [
+  //     { url: "/assets/images/favicon.ico" },
+  //     { url: "/assets/images/favicon-web-32.png", sizes: "32x32", type: "image/png" },
+  //     { url: "/assets/images/favicon-web-16.png", sizes: "16x16", type: "image/png" },
+  //   ],
+  //   apple: [{ url: "/assets/images/favicon-web-180.png", sizes: "180x180", type: "image/png" }],
+  //   shortcut: ["/assets/images/favicon.ico"],
+  // },
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
